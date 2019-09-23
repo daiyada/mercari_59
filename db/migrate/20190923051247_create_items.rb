6565acc7866a7050920b35edf793ff0f2c2c5e27
daiyada <shunsuke07l6@gmail.com>
2019-09-23 15:45:32 +0900

    1st-commit

--- db/migrate/20190923051247_create_items.rb
+++ db/migrate/20190923051247_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.references :user,         foreign_key: true, null: false
+      t.references :category,     foreign_key: true, null: false
+      t.string :name,             null: false, index: true
+      t.text :escript,            null: false
+      t.string :condition,        null: false
+      t.integer :price,           null: false
+      t.integer :stock_status,    null: false
+      t.string :size,             null: false
+      t.string :brand
       t.timestamps
     end
   end
