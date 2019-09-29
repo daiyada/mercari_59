08fb76bfb24eea142f7b80e81a3426f7ba237675
ksk1001 <bzoj111@gmail.com>
2019-09-29 17:58:59 +0900

    routesのusersにnewを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "example#index"
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit, :new]
   resources :items, only: [:show, :new , :index] do
     collection do
       get "purchase/:id" => "items#purchase"
