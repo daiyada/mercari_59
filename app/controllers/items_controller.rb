e69eef35a15ebdf6cdbfab546e832a804da44420
mi-tomo <efficientcycle@gmail.com>
2019-09-30 21:09:29 +0900

    mi-出品した商品のdbへの登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,33 +4,25 @@ class ItemsController < ApplicationController
   end
   def new
     @item = Item.new
-    @image = Image.new
-    # @categorie = Category.new
-    @deliverrie = Delivery.new
+    # @image = Image.new
+    # @deliverrie = Delivery.new
   end
   def create
-    params_of_item = item_params
-    params_of_image = image_params
-    params_of_delivery = delivery_params
-    # binding.pry
-    item = Item.new(params_of_item) 
-    
-  
-    delivery = Delivery.new(params_of_delivery)
-    image = Image.new(params_of_image) 
-    image.save
-    delivery.save
-      binding.pry
-    if  item.save
+
+    item = Item.new(item_params) 
+    delivery = Delivery.new(delivery_params)
+    image = Image.new(image_params) 
+    validate = [image.image , item.name , item.descript , item.condition , item.price , delivery.pay_for_shipping , delivery.delivery_from , delivery.due_time_day ]
+    unless validate.include?(""||nil)
+      item.save  
+      image.save
+      delivery.save
       redirect_to controller: :items, action: :index
-    else  
-      render :new
-    end
-      # item = Item.create(:item params[:id])
-    # item.images.create(:item params[:id],:image params[:image])
-    # item.categorie.create(:ancestory params[:ancestory],:name params[:name])
-    # item.deliverrie.create(:due_time_day params[:due_time_day],:delivery_from params[:delivery_from], :due_time_day params[:due_time_day])
+    else
+    # render :new
     # redirect_to controller: :items, action: :new
+    end
+ 
   end
   def show
   end
@@ -38,20 +30,18 @@ class ItemsController < ApplicationController
   end
   private
   def item_params
-    # params.require(:group).permit(:name, { :user_ids => [] })
-    # params.permit(:image, :text)
     params.require(:item).permit(:name,:descript,:condition,:price).merge(user_id: 1,stock_status: 1, category_id: 3, size:"M")
-    # (user_id: current_user.id)
-    
-    # binding.pry
   end
   def image_params
     item_id = Item.last
-    item_id = item_id.id.to_i + 1 
-    params.require(:image).permit(:image).merge(item_id: item_id.to_s)
+    if item_id == nil
+      item_id =1 
+    else
+      item_id = item_id.id + 1
+    end
+    params.require(:image).permit(:image).merge(item_id: item_id)
   end
   def delivery_params
     params.require(:delivery).permit(:pay_for_shipping,:delivery_from,:due_time_day)
-    # binding.pry
   end
 end
