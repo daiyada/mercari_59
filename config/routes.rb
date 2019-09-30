105f20bc44f2176069304b29cf5d364c0e1b1446
daiyada <shunsuke07l6@gmail.com>
2019-09-30 14:08:34 +0900

    ログアウト画面を表示するためのルーティングとコントローラの設定をしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "example#index"
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit] do
+    member do
+      get "logout" => "users#logout"
+    end
+  end
   resources :cards, only: [:show]
   resources :items, only: [:show, :new , :index] do
     collection do
