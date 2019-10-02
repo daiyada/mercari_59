46d99237801b1186bda6eb35b52085e7e1b6c4a8
mi-tomo <efficientcycle@gmail.com>
2019-10-02 09:32:40 +0900

    カテゴリーの実装（料金計算前）

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
     collection do
       get "purchase/:id" => "items#purchase"
     end
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
   end
 end
