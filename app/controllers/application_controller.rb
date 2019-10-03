814a7991a481ee24f8635475d7af6a0cf5a8a089
ksk1001 <bzoj111@gmail.com>
2019-10-03 13:21:06 +0900

    新規登録情報の内、userテーブル分のみ保存可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:cellphone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first_kana])
+  end
 end
