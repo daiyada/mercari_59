65b47a6ec05939b8d38932d87099258bbfa8dc98
daiyada <shunsuke07l6@gmail.com>
2019-10-04 12:36:29 +0900

    コントローラを追記しました。

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,7 +10,12 @@ class CardsController < ApplicationController
 
   def create
     Payjp.api_key = Rails.application.credentials.dig(:payjp,:PAYJP_SECRET_KEY)
-    customer = Payjp::Customer
-    binding.pry
+    customer = Payjp::Customer.create(card: params[:payjpToken])
+    @card = Card.new(user_id: current_user&.id, customer_id: customer.id, card_id: params[:payjpToken])
+    if @card.blank?
+      redirect_to "/cards/1/edit"
+    else
+      redirect_to "/cards/1/"
+    end
   end
 end
