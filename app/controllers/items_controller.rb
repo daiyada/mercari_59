a616bd2a9e93e8abf707a2494876bbfaeb7fefe6
mi-tomo <efficientcycle@gmail.com>
2019-10-05 19:03:19 +0900

    form_with使ってみる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,12 @@
 class ItemsController < ApplicationController
   def index
-    
+     # @images = Image.includes(:item).per(10).order("created_at DESC")
+     @image = Image.find(14)
   end
   def new
     @item = Item.new
+    @item.images.build
+    # @image = Image.new
 
 
 
@@ -14,30 +17,34 @@ class ItemsController < ApplicationController
     end
   end
   def create
-    
-    item = Item.new(item_params) 
-    delivery = Delivery.new(delivery_params)
-    item_id = Item.last
-    item_id == nil ? item_id =1 : item_id = item_id.id + 1
-    image_params_pics = image_params.compact.reject(&:empty?)
-    image = Image.new(image: image_params[0] , item_id: item_id) 
 
-    validate = [image.image , item.name , item.descript , item.condition , item.price ,item.category_id, delivery.pay_for_shipping , delivery.delivery_from , delivery.due_time_day ]
+
+    # binding.pry
+        item_id = Item.last
+        item_id == nil ? item_id =1 : item_id = item_id.id + 1
+    item = Item.new(item_params) 
+    
+    # item.save
+    
+    delivery = Delivery.new(delivery_params)
+   
+
+    validate = [ item.name , item.descript , item.condition , item.price ,item.category_id, delivery.pay_for_shipping , delivery.delivery_from , delivery.due_time_day ]
     
     unless validate.include?(""||nil)
       item.save  
-      image.save
+      # image.save
       delivery.save
      
-      image_params_pics.drop(1).each do |photo|
-      image = Image.new(image: photo, item_id: item_id) unless photo == ""
-      image.save
+      # image_params_pics.drop(1).each do |photo|
+      # image = Image.new(image: photo, item_id: item_id) unless photo == ""
+      # image.save
       
-        end
-      redirect_to controller: :items, action: :index
+      #   end
+      # redirect_to controller: :items, action: :index
     else
-    # render :new
-    # redirect_to controller: :items, action: :new
+    render :new
+    redirect_to controller: :items, action: :new
     end
   end
   def get_category_children 
@@ -57,12 +64,16 @@ class ItemsController < ApplicationController
     ancestry_pass = params.require(:grandchild_id) rescue
     ancestry_pass =nil if ancestry_pass == "---" || ancestry_pass == nil || ancestry_pass == "" 
 
-    params.require(:item).permit(:name,:descript,:condition,:price).merge(buyer_id: 0, seller_id: 1,stock_status: 1, category_id: ancestry_pass, size:"M")
+    params.require(:item).permit(:name,:descript,:condition,:price, images_attributes: {image: []} ).merge(buyer_id: 0, seller_id: 1,stock_status: 1, category_id: ancestry_pass, size:"M")
+    # binding.pry
   end
   def image_params
-    # params.require(:image).permit(:image1,:image2,:image3,:image4,:image5,:image6,:image7,:image8,:image9,:image10).merge(item_id: item_id)
-    params.require(:image)
-    # params.require(:image).permit({image: []}).merge(item_id: item_id)
+    # params.require(:image).permit(:image1,:image2,:image3,:image4,:image5,:image6,:image7,:image8,:image9,:image10).merge(item_id: item_id) images_attributes: [:image]
+    # params.require(:image)
+    
+    params.require(:image) 
+    
+    # .merge(item_id: item_id) 
   end
   def delivery_params
     params.require(:delivery).permit(:pay_for_shipping,:delivery_from,:due_time_day)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
 
-  has_many :images, dependent: :destroy
+  has_many :images ,inverse_of: :item #, dependent: :destroy
   has_one :delivery, dependent: :destroy
   belongs_to :category
   
   # belongs_to :saler, class_name: "User"
   # belongs_to :buyer, class_name: "User"
-
+  accepts_nested_attributes_for :images
 end
