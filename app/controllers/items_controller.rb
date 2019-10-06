d3a259b59f4a3b3845106a5ca822922889b43f94
mi-tomo <efficientcycle@gmail.com>
2019-10-06 16:03:52 +0900

    コントローラコメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   def index
-    #  @images = Image.last.includes(:item).per(10).order("created_at DESC")
-     @image = Image.last
-    #  binding.pry
+     @images = Image.includes(:item).per(10).order("created_at DESC")
+    
   end
   def new
     @item = Item.new
@@ -10,23 +9,20 @@ class ItemsController < ApplicationController
     @category = Category.all
     @category_parent_array = ["---"]
     Category.where(ancestry: nil).each do |parent|
-        @category_parent_array << parent.name
+    @category_parent_array << parent.name
     end
   end
   def create
 
-        item_id = Item.last
-        item_id == nil ? item_id =1 : item_id = item_id.id + 1
+    item_id = Item.last
+    item_id == nil ? item_id =1 : item_id = item_id.id + 1
     item = Item.new(item_params) 
     image_params_pics = params[:item][:images_attributes][:"0"][:image] rescue
     image = Image.new(item_id: item_id) 
     image.image = image_params_pics[0] unless image_params_pics = ""
-   
-    
     delivery = Delivery.new(delivery_params)
 
     validate = [ image.image, item.name , item.descript , item.condition , item.price ,item.category_id, delivery.pay_for_shipping , delivery.delivery_from , delivery.due_time_day ]
-    
     unless validate.include?("") || validate.include?(nil)
       item.save  
       image.save
@@ -36,9 +32,7 @@ class ItemsController < ApplicationController
         image.save
       end
       redirect_to controller: :items, action: :index
-    else
-    # render :new
-    # redirect_to controller: :items, action: :new
+    
     end
   end
   def get_category_children 
@@ -47,21 +41,18 @@ class ItemsController < ApplicationController
   
   def get_category_grandchildren
     @category_grandchildren = Category.find("#{params[:child_id]}").children
- end
+  end
 
   def show
   end
   def purchase
   end
   private
-  def item_params
-    
+  def item_params  
     ancestry_pass = params.require(:grandchild_id) rescue
     ancestry_pass =nil if ancestry_pass == "---" || ancestry_pass == nil || ancestry_pass == "" 
     params.require(:item).permit(:name,:descript,:condition,:price ).merge(buyer_id: 0, seller_id: 1,stock_status: 1, category_id: ancestry_pass, size:"M")
   end
-
-
 
   def delivery_params
     params.require(:delivery).permit(:pay_for_shipping,:delivery_from,:due_time_day)
