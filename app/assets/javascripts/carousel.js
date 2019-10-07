0de095d6b57129640b1d8b609d1f5987635c6408
mi-tomo <efficientcycle@gmail.com>
2019-10-07 17:51:13 +0900

    トップページスリック実装

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay:true,
+    dots:true,
+    prevArrow: '<img src="https://web-jp-assets.mercdn.net/_next/static/images/carousel-prev-af7ca886ce42c4c02610cd15421ddcad.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="https://web-jp-assets.mercdn.net/_next/static/images/carousel-next-bc3a1438a1435e953bc67d720cd05c61.png" class="slide-arrow next-arrow">'
+    
+  });
+ 
+});
