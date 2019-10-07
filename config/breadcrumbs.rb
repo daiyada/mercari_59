62c76c125bd0c597589f055afcf627a0e3f5c5ba
daiyada <shunsuke07l6@gmail.com>
2019-10-07 13:05:18 +0900

    作成したページにパンくず機能を実装しました。

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,28 @@
 crumb :root do
-  link "Home", root_path
+  link "メルカリ", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :mypage do
+  link "マイページ", users_path
+  parent :root
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :userinfo do
+  link "本人情報の登録", info_user_path
+  parent :mypage
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :regicard do
+  link "支払い方法", new_card_path
+  parent :mypage
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
+crumb :logout do
+  link "ログアウト", new_card_path
+  parent :mypage
+end
 
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :cardinfo do
+  link "クレジットカード情報入力", edit_card_path
+  parent :regicard
+end
