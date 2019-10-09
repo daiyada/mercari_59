cced5914a38ae9659062f74a7c2c8968ed526a6e
daiyada <shunsuke07l6@gmail.com>
2019-10-09 17:14:34 +0900

    テスト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
 //= require jquery.turbolinks
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
