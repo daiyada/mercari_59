b3c017ed0a170b5b88636e2ecd91faab688864c6
ksk1001 <bzoj111@gmail.com>
2019-10-13 19:52:16 +0900

    devise_parameter_sanitizerを記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:cellphone_number])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family_kana])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first_kana])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:cellphone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first_kana])
 
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:prefecture])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:building])
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
-  # end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prefecture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:building])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+  end
 end
