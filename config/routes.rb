8035ab0857905f0409e929c51f459c0de5c0f467
ksk1001 <bzoj111@gmail.com>
2019-10-13 15:58:05 +0900

    registration関係を削除

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   end
 
   # devise_for :users, :controllers => {
-  #   :registrations => 'users/registrations',
   #   :sessions => 'users/sessions',
   #   :omniauth_callbacks => 'users/omniauth_callbacks'
   # } 
