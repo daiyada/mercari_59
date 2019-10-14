4b647b7ae3d14da2187d87ad7bf1cda2f7e56eeb
ksk1001 <bzoj111@gmail.com>
2019-10-14 21:44:01 +0900

    user,addressテーブルのみにバリデーション設定
    モデルにてバリデーションを設定、ページ移動毎にバリデーションに適合しているかを確認し、適合しない場合は次のページに進めず、不適合箇所にエラーメッセージが表示される。

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  #STEP3入力項目validation
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  # validates :building
+  # validates :phone_number
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
+
+  #STEP1入力項目validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: {minimum: 7, maximum: 128}
+  validates :name_family, presence: true
+  validates :name_first, presence: true
+  validates :name_family_kana, presence: true
+  validates :name_first_kana, presence: true
+
+  #STEP2入力項目validation
+  validates :cellphone_number, presence: true
+
 end
