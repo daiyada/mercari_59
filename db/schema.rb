164de72c1f2c8845678f5edd0f451bb8e4c2d395
ksk1001 <bzoj111@gmail.com>
2019-10-14 12:40:21 +0900

    環境設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_093753) do
+ActiveRecord::Schema.define(version: 2019_10_14_031548) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2019_10_07_093753) do
     t.index ["name"], name: "index_items_on_name"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -100,4 +109,5 @@ ActiveRecord::Schema.define(version: 2019_10_07_093753) do
   add_foreign_key "deliveries", "items"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
+  add_foreign_key "sns_credentials", "users"
 end
