bc257f942c60811880ddb06db686b380a6273084
daiyada <shunsuke07l6@gmail.com>
2019-10-19 14:46:11 +0900

    タブの切り替えをできるようにしました。

--- app/assets/javascripts/switch_tab.js
+++ app/assets/javascripts/switch_tab.js
@@ -1,10 +1,21 @@
 $(document).on("turbolinks:load",function(){
+  // マイページ下部のタブ切り替え
   $(".hi-container__wrapper__main__right__itemlist__tab__switch").on("click", function(){
-    $('.is-shown').removeClass('is-shown');
-    console.log(this);
+    $('.hi-container__wrapper__main__right__itemlist__tab__switch.is-shown').removeClass('is-shown');
 		$(this).addClass('is-shown');
-    $('.is-active').removeClass('is-active');
-    const index = $(this).index();
+    $('.hi-container__wrapper__main__right__itemlist__box.is-active').removeClass('is-active');
+    var index = $(this).index();
     $('.hi-container__wrapper__main__right__itemlist__box').eq(index).addClass('is-active');
+  })
+
+  // マイページ上部のタブの切り替え
+  $(".hi-container__wrapper__main__right__tab__switch").on("click", function(){
+    $('.hi-container__wrapper__main__right__tab__switch.is-shown').removeClass('is-shown');
+    $(this).addClass('is-shown');
+    var index = $(this).index();
+    $('.hi-container__wrapper__main__right__list.is-active').css('display','none');
+    $('.hi-container__wrapper__main__right__list.is-active').removeClass('is-active');
+    $('.hi-container__wrapper__main__right__list').eq(index).addClass('is-active');
+    $('.hi-container__wrapper__main__right__list.is-active').css('display','block');
   })
 })
